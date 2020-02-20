8165fe0244291d691340ea0c3aab876ab2f6a247
Isabella Ramirez <irborrero@gmail.com>
2020-02-20 18:53:40 -0500

    Draw pannel added

--- src/components/CourseTable/CourseTableComponent.js
+++ src/components/CourseTable/CourseTableComponent.js
@@ -2,22 +2,26 @@ import React from "react";
 import CourseRowComponent from "./CourseRowComponent";
 
 const CourseTableComponent = ({courses, deleteCourse, activeRow, editingRow, selectRow, editRow}) =>
-    <tbody>
-    {
-        courses.map(function(course, index){
-            return (
-                <CourseRowComponent
-                    course={course}
-                    index={index}
-                    deleteCourse={deleteCourse}
-                    activeRow={activeRow}
-                    editingRow={editingRow}
-                    selectRow = {selectRow}
-                    editRow = {editRow}
-                />
-            )
-        })
-    }
-    </tbody>
+    <React.Fragment>
+        <tbody>
+        {
+            courses.map(function(course, index){
+                return (
+                    <CourseRowComponent
+                        course={course}
+                        index={index}
+                        deleteCourse={deleteCourse}
+                        activeRow={activeRow}
+                        editingRow={editingRow}
+                        selectRow = {selectRow}
+                        editRow = {editRow}
+                    />
+                )
+            })
+        }
+        </tbody>
+    </React.Fragment>
+
+
 
 export default CourseTableComponent
