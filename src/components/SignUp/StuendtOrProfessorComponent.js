c317ac994e3b75da2cb2b7cb0740ccc2c8f4a816
Isabella Ramirez <irborrero@gmail.com>
2020-03-08 19:16:04 -0400

    Style added

--- src/components/SignUp/StuendtOrProfessorComponent.js
+++ src/components/SignUp/StuendtOrProfessorComponent.js
@@ -1,16 +1,30 @@
 import React from "react";
+import "./SignUp.style.client.css"
+import {Link} from "react-router-dom";
+
 const StudentOrProfessorComponent = () =>
-    <div className="container">
-        <h1>Sign Up</h1>
-        <h3>I am a ...</h3>
-        <div>
-            <button>
-                Professor
-            </button>
-            <button>
-                Student
-            </button>
-        </div>
-    </div>
+        <React.Fragment>
+            <h1>Sign Up</h1>
+            <div className="container-signup">
+                <h2>I am a</h2>
+                <div>
+                    <div>
+                        <Link to="/signup/professor">
+                            <button className="btn btn-block btn-signup">
+                                Professor
+                            </button>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link to="/signup/form">
+                            <button className="btn btn-block btn-signup">
+                                Student
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+
 
 export default StudentOrProfessorComponent
