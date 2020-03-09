ec5a03a478524ea7021c317d2a4ef104a3a6914f
Guillermo del Aguila <gdelagui@purdue.edu>
2020-03-09 17:57:41 -0400

    course page template and layout

--- src/components/CoursePage/ConversationsComponent.js
+++ src/components/CoursePage/ConversationsComponent.js
@@ -4,7 +4,89 @@ class ConversationsComponent extends React.Component{
 
     render() {
         return(
-            <h1>CONVERSATION COMPONENT</h1>
+            <div className="container-fluid text-left">
+                <div className="row">
+                    <div className="col-3">
+
+                        <div className="overflow-auto">
+                            <div className="list-group list-group-flush">
+                                <text className="list-group-item">
+                                    <h4>CONVERSATION TOPICS</h4>
+                                </text>
+                                <a href="#" className="list-group-item list-group-item-action">
+                                    Grading
+                                </a>
+                                <a href="#" className="list-group-item list-group-item-action">
+                                    Work Load
+                                </a>
+                                <a href="#" className="list-group-item list-group-item-action">
+                                    Instructor
+                                </a>
+                                <a href="#" className="list-group-item list-group-item-action">
+                                    Content
+                                </a>
+                                <a href="#" className="list-group-item list-group-item-action">
+                                    Prequesites
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-9">
+                        <div className="list-group">
+                            <text className="list-group-item border-0">
+                                <h4>DISCUSSIONS FOR TOPIC</h4>
+                            </text>
+
+                        </div>
+
+                        <div className="list-group">
+                            <a href="#"
+                               className="list-group-item list-group-item-action flex-column align-items-start">
+                                <div className="d-flex w-100 justify-content-between">
+                                    <h5 className="mb-1"> I had a great experience!!</h5>
+                                    <small>Today</small>
+                                </div>
+                                <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam
+                                    eget risus varius blandit.</p>
+                                <small>By Ana</small>
+                            </a>
+                            <a href="#"
+                               className="list-group-item list-group-item-action flex-column align-items-start">
+                                <div className="d-flex w-100 justify-content-between">
+                                    <h5 className="mb-1">Great class but a lot of work...</h5>
+                                    <small className="text-muted">1 days ago</small>
+                                </div>
+                                <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam
+                                    eget risus varius blandit.</p>
+                                <small className="text-muted">By Jake</small>
+                            </a>
+                            <a href="#"
+                               className="list-group-item list-group-item-action flex-column align-items-start">
+                                <div className="d-flex w-100 justify-content-between">
+                                    <h5 className="mb-1">Very useful!!</h5>
+                                    <small className="text-muted">3 days ago</small>
+                                </div>
+                                <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam
+                                    eget risus varius blandit.</p>
+                                <small className="text-muted">By Dona</small>
+                            </a>
+                            <a href="#"
+                               className="list-group-item list-group-item-action flex-column align-items-start">
+                                <div className="d-flex w-100 justify-content-between">
+                                    <h5 className="mb-1">Could have been better organized</h5>
+                                    <small className="text-muted">3 days ago</small>
+                                </div>
+                                <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam
+                                    eget risus varius blandit.</p>
+                                <small className="text-muted">By Ben</small>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
         )
     }
 
--- src/components/CoursePage/InstructorComponent.js
+++ src/components/CoursePage/InstructorComponent.js
@@ -4,7 +4,27 @@ class InstructorComponent extends React.Component{
 
     render() {
         return(
-            <h1>INSTRUCTOR COMPONENT</h1>
+            <div className="container-fluid border text-left">
+                <div className="row">
+                    <h1>INSTRUCTOR COMPONENT</h1>
+                </div>
+                <div className="row">
+                    <div className="col-3 ">
+                        <h3>Instructor Picture</h3>
+
+                    </div>
+                    <div className="col-9">
+                        <h3>Brief description</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                            irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                            deserunt mollit anim id est laborum."</p>
+                    </div>
+                </div>
+            </div>
+
         )
     }
 
